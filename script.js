e4e0a8d5580887e475e946e723162bcdea2f38e4
cell1303 <mailangkayangelic@gmail.com>
2025-03-25 16:43:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function validateNumber() {
-    let input = document.getElementById("numberInput").value;
-    let regex = /^-?\\d+(\\.\\d+)?(e[-+]?\\d+)?$/i;
+    let input = document.getElementById("numberInput").value.trim();
+    
+    // Pola regex untuk angka biasa dan scientific notation
+    let regex = /^-?\d+(\.\d+)?(e[-+]?\d+)?$/i;
+    
     let resultText = regex.test(input) ? "Yes, it is a number" : "No, it is not a number";
     document.getElementById("result").innerText = resultText;
 }
